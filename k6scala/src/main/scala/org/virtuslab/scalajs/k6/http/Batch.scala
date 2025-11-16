b935e6f482d6b1e9c0efbd2ab003e9496b0f13c2
Michał Wiącek <wiacekm@virtuslab.com>
2025-11-16 21:10:30 +0100

    fix compilation and add some improvements
    1. modify build.sbt so it support version specific sources
    2. add version specific sources so it compiles correctly
    3. stop using string literal in favor of sealed trait
    4. bump sbt plugin version
    5. add Batch method
    6. implement asyncReqeust properly
    7. provide examples for new features

---
+++ k6scala/src/main/scala/org/virtuslab/scalajs/k6/http/Batch.scala
@@ -0,0 +1,54 @@
+package org.virtuslab.scalajs.k6.http
+
+import scala.scalajs.js
+import scala.scalajs.js.|
+import scala.scalajs.js.typedarray.ArrayBuffer
+import scala.scalajs.js.JSConverters._
+
+type RequestBody = String | StructuredRequestBody | ArrayBuffer
+
+@js.native
+trait StructuredRequestBody extends js.Object
+
+object StructuredRequestBody {
+  def apply(fields: Map[String, String | FileData]): StructuredRequestBody = {
+    val obj = js.Dynamic.literal()
+    fields.foreach { case (key, value) =>
+      obj.updateDynamic(key)(value.asInstanceOf[js.Any])
+    }
+    obj.asInstanceOf[StructuredRequestBody]
+  }
+}
+
+type ArrayBatchRequest = js.Tuple4[String, URL, js.UndefOr[RequestBody], js.UndefOr[Params]]
+
+@js.native
+trait ObjectBatchRequest extends js.Object {
+  val method: String = js.native
+  val url: URL = js.native
+  val body: js.UndefOr[RequestBody] = js.native
+  val params: js.UndefOr[Params] = js.native
+}
+
+object ObjectBatchRequest {
+  def apply(
+      method: String,
+      url: URL,
+      body: Option[RequestBody] = None,
+      params: Option[Params] = None
+  ): ObjectBatchRequest =
+    js.Dynamic
+      .literal(
+        method = method,
+        url = url.asInstanceOf[js.Any],
+        body = body.orUndefined.asInstanceOf[js.Any],
+        params = params.orUndefined
+      )
+      .asInstanceOf[ObjectBatchRequest]
+}
+
+type BatchRequest = String | HttpURL | ArrayBatchRequest | ObjectBatchRequest
+
+type BatchRequests = js.Array[BatchRequest] | js.Dictionary[BatchRequest]
+
+type BatchResponses = js.Array[Response] | js.Dictionary[Response]
--- k6scala/src/main/scala/org/virtuslab/scalajs/k6/http/package.scala
+++ k6scala/src/main/scala/org/virtuslab/scalajs/k6/http/package.scala
@@ -3,6 +3,7 @@ package org.virtuslab.scalajs.k6
 import scala.scalajs.js
 import scala.scalajs.js.|
 import js.JSConverters._
+import scala.scalajs.js.JSConverters._
 import scala.scalajs.js.typedarray._
 import org.virtuslab.scalajs.converters.FromJSPromise
 import org.virtuslab.scalajs.k6.http.AsyncRequest
@@ -21,6 +22,16 @@ package object http {
       body: Option[String] = None, // TODO add other formats
       params: Option[Params] = None
   ): F[Response] = AsyncRequest.asyncRequest(method, url, body, params)
+  def request(
+      method: HttpMethod,
+      url: URL,
+      body: Option[String] = None, // TODO add other formats
+      params: Option[Params] = None
+  ): Response = Http.request(method.toJSType, url, body.orUndefined, params.orUndefined)
+  def batch(requests: Seq[BatchRequest]): Seq[Response] =
+    Http.batch(requests.toJSArray.asInstanceOf[BatchRequests]).asInstanceOf[js.Array[Response]].toSeq
+  def batch(requests: Map[String, BatchRequest]): Map[String, Response] =
+    Http.batch(requests.toJSDictionary.asInstanceOf[BatchRequests]).asInstanceOf[js.Dictionary[Response]].toMap
   def cookieJar(): CookieJar =
     Http.cookieJar()
   def del(url: URL, body: Option[String] = None, params: Option[Params] = None): Response =
